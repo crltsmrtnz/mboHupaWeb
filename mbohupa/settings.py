821f200b1f52c59f7a6f294931de582440f66ed6
crlts <crltsmrtnz@gmail.com>
2019-06-20 19:12:38 -0400

    se cambio el debug a True

--- mbohupa/settings.py
+++ mbohupa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c^49-(1evngfwus_f^%ez*t1*%=gqezcy&x^dp0ptt@qa0-*^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
